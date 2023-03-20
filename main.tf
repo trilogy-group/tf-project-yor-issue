f5d9982557d6254f31d09812a0deb864c7a8f9e3
Yogesh Kumar <yogesh.kumar@trilogy.com>
2023-03-20 21:46:41 +0530

    Add terraform changes

--- main.tf
+++ main.tf
@@ -12,11 +12,24 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+locals {
+  stage = "dev"
+  public_instance_name = "public-instance"
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "${local.stage}-test-bucket"
+  tags = {
+    Name                        = "${local.stage}-respond-test-bucket"
+    yor_trace                   = "37fb80ff-18cd-4d83-a6fa-59049653877b"
+  }
+}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = local.public_instance_name
   }
 }
